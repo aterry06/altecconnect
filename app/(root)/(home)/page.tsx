844ac82911dc0c41c5e07a8cabbb5c12dd85d4ae
Allan Terry <alankterry@gmail.com>
2024-04-14 12:44:08 -0600

    latest

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,14 +9,14 @@ const Home = () => {
 
   const time = moment().format('h:mm A');
   const date = moment().format('MMMM Do, YYYY');
- 
+  
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[600px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting : View scheduled meetings on the Upcoming Page
+            Upcoming Meeting: View scheduled Meetings on Upcoming Page
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
